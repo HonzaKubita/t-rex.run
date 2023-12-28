fabf54d2dbc7907a3c6f7e168957640024c228a7
Honza Kubita <honza.kubita@gmail.com>
2023-12-28 18:16:12 +0100

    Fixed pinia

--- client/stores/mainStore.js
+++ client/stores/mainStore.js
@@ -6,20 +6,24 @@ export const useMainStore = defineStore('mainStore', {
       players: [],
   
     }),
-    
-    addPlayer(player){
-      this.players.push(player)
+
+    getters: {
+        getPlayerById(playerId) {
+            return this.players.find(player => player.id == playerId)
+        },
     },
 
-    addPlayers(players){
-      this.players.extend(players)
+    actions: {
+        addPlayer(player){
+            this.players.push(player)
+          },
+      
+          addPlayers(players){
+            this.players.push(...players)
+          },
+      
+          removePlayer(playerId) {
+              this.players = this.players.filter(player => player.id != playerId)
+          },
     },
-
-    removePlayer(playerId) {
-        this.players = this.players.filter(player => player.id != playerId)
-    },
-
-    getPlayerById(playerId) {
-        return this.players.find(player => player.id == playerId)
-    }
-  })
+  });
