ac638532398ea3a708878f72b165e2d61f0aa1f4
Honza Kubita <honza.kubita@gmail.com>
2023-12-28 16:05:50 +0100

    Added github link

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
     <NuxtPage />
+
+    <a href="https://github.com/HonzaKubita/t-rex.run" target="_blank" class="github-link">
+        <img src="/github.svg">
+    </a>
   </div>
 </template>
+
+
+<style>
+
+.github-link {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+
+    width: 50px;
+    height: 50px;
+}
+
+</style>
