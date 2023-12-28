00a55541052ec560e717f4f69885e6ffdc416ca2
Honza Kubita <honza.kubita@gmail.com>
2023-12-28 16:04:11 +0100

    Added 404 page

---
+++ pages/[...404].vue
@@ -0,0 +1,28 @@
+<template>
+<div class="notfound">
+
+    <img src="/404.png" alt="404">
+    <h1>404 Not Found!</h1>
+    <NuxtLink to="/" class="button">Back to home</NuxtLink>
+
+</div>
+</template>
+
+<style>
+
+.notfound {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.notfound > .button {
+    margin-top: 20px;
+    text-decoration: none;
+}
+
+</style>
