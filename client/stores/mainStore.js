4f90a1c5a3d168ba9000320e7cad2447384ad217
Honza Kubita <honza.kubita@gmail.com>
2023-12-28 18:40:47 +0100

    Added missing semicolons

--- client/stores/mainStore.js
+++ client/stores/mainStore.js
@@ -9,21 +9,21 @@ export const useMainStore = defineStore('mainStore', {
 
     getters: {
         getPlayerById(playerId) {
-            return this.players.find(player => player.id == playerId)
+            return this.players.find(player => player.id == playerId);
         },
     },
 
     actions: {
         addPlayer(player){
-            this.players.push(player)
+            this.players.push(player);
         },
       
         addPlayers(players){
-            this.players.push(...players)
+            this.players.push(...players);
         },
     
         removePlayer(playerId) {
-            this.players = this.players.filter(player => player.id != playerId)
+            this.players = this.players.filter(player => player.id != playerId);
         },
     },
   });
