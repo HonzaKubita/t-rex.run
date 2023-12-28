cd9dacadae85d782337088b79edd1bcb1ceff133
Honza Kubita <honza.kubita@gmail.com>
2023-12-28 14:14:20 +0100

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  ssr: false,
+  app: {
+    head: {
+      title: "T-rex Multiplayer",
+      htmlAttrs: {
+        lang: 'en'
+      },
+    }
+  },
 })
