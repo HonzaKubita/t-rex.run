a4c17b6401bbb53aa55536d20fe3848dd6bbce5d
Honza Kubita <honza.kubita@gmail.com>
2024-01-03 22:05:00 +0100

    Enabled static files serving

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const settings = require('./modules/settings');
 // Create an Express app
 const app = express();
 
-// app.use(express.static("../client/dist"));
+app.use(express.static("../client/dist"));
 
 const server = app.listen(settings.listenPort, () => {
     console.log(`Server listening on port ${settings.listenPort}`);
